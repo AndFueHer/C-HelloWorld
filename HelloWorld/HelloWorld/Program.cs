3e76272702bbdea5fd8d22011b6bf295ae162497
Fuentes <andres.fuentes@covidien.com>
2016-07-21 17:44:38 -0600

    Hello World en C#

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi World!\n");
+            Console.WriteLine("Press any key to get out of here!!!");
+            Console.ReadKey();
+        }
+    }
+}
